e88bcfea288694a9fdc046b4844428a6320d44f9
argaitt <argaitt@gmail.com>
2018-09-30 17:39:10 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  function div(val, by){
+    return (val - val % by) / by;
+  }
+  let s = 0; 
+  while(number > 5) {
+    s = s + div(number, 5); 
+    number = div(number,5);
+ }
+ return s;
 }
